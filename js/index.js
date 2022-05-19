4f23f8cab5f78449ac5b957c3a61ecd82c026a49
akbarjon07 <akbarjon.1019@mail.ru>
2022-05-19 21:02:37 +0300

    js questions

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var userName = prompt("What is your full name?", "Michael Jackson");
+
+var userAge = prompt("How old are you?", "20");
+
+var userStudy = prompt("Where do you study?", "Westminster");
+
+var userFaculty = prompt("What is your faculty?", "Business Administartion");
+
+var userHobby = prompt("What are you interested in?", "IT");
+
+var userHobbyType = prompt("What type of it ?", "Web Developing");
+
+var userLanguage = prompt("What kind of programming languages you know?", "JavaScript");
+
+var userJob = prompt("Where do you want to work?", "Amazon");
+
+var userOpinion = prompt("What is your future plans?", "Typing");
